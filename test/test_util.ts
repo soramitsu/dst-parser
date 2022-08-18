f30df9e20db3604d8c3d56a3587951669a0b3612
6r1d <vic.6r1d@gmail.com>
2022-08-18 20:40:17 +0300

    [refactor] Simplifies tests

--- test/test_util.ts
+++ test/test_util.ts
@@ -1,21 +1,16 @@
 import * as fs from "fs"
 import { ExampleParser } from "../src/index"
 
-export function parseFile (txtPath: string) {
-    // Load a text to test from the FS
-    let text: string = ''
-    try {
-        text = fs.readFileSync(txtPath, 'utf8').toString()
-    } catch (e: any) {
-        console.log('Error:', e.stack)
-    }
+export function parseStr (text: string) {
     // Initialize the parser
     let parser_inst: ExampleParser = new ExampleParser(text)
     return parser_inst.mapLines()
 }
 
-export function parseStr (text: string) {
+export function parseFile (txtPath: string) {
+    // Load a text to test from the FS
+    let text = ''
+    text = fs.readFileSync(txtPath, 'utf8').toString()
     // Initialize the parser
-    let parser_inst: ExampleParser = new ExampleParser(text)
-    return parser_inst.mapLines()
+    return parseStr(text)
 }
